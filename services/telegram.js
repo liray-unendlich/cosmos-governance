57a2ef5359d0738e20650f8b3d689d56d1e6ab06
liray-unendlich <unendlichkeit629@gmail.com>
2024-02-10 12:53:49 +0900

    Locale Change

--- services/telegram.js
+++ services/telegram.js
@@ -1,4 +1,5 @@
 import TelegramBot from "node-telegram-bot-api";
+import { format } from 'date-fns';
 import { TelegramToken, TgChannelId } from "../config/env.js";
 
 const telegram = new TelegramBot(TelegramToken, {
@@ -15,7 +16,7 @@ export async function sendMessageToTelegram(chatId, message) {
 
 export async function sendProposalToTelegram(proposal, chain) {
 	const cliMessage = `\`\`\`bash ${chain.binaryName} tx gov vote ${proposal.id} [yes/no/no_with_veto/abstain] --chain-id ${chain.chainId} --from [your_key_name]\`\`\``;
-	let messageText = `**${chain.name}'s New Proposal!**\n${proposal.title}\n${proposal.description}`;
+	let messageText = `**${chain.name}で新しいプロポーザルが提案されました!**\n${proposal.title}\n${proposal.description}`;
 
 	// check the message length
 	if (messageText.length > 1024) {
@@ -23,13 +24,13 @@ export async function sendProposalToTelegram(proposal, chain) {
 	}
 
 	// add proposal information to message
-	messageText += `\n\n*Proposal ID*: ${
+	messageText += `\n\n*プロポーザルID*: ${
 		proposal.id
-	}\n*Submit Time*: ${proposal.submitTime.toUTCString()}\n*Voting Time*: ${proposal.votingStartTime.toUTCString()} - ${proposal.votingEndTime.toUTCString()}\n*How to vote via CLI?*:\n${cliMessage}`;
+	}\n*提案時間*: ${format(proposal.submitTime, 'yyyy/MM/dd HH:mm')}\n*投票期間*: ${format(proposal.votingStartTime, 'yyyy/MM/dd HH:mm')} - ${format(proposal.votingEndTime, 'yyyy/MM/dd HH:mm')}\n*CLIでの投票方法*:\n${cliMessage}`;
 
 	if (chain.proposalUrl) {
 		const url = chain.proposalUrl.replace("{id}", proposal.id);
-		messageText += `\n\n[Proposal Link](${url})`;
+		messageText += `\n\n[プロポーザルリンク](${url})`;
 	}
 
 	await sendMessageToTelegram(TgChannelId, messageText);
