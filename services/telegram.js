459e2aeaa0812f6f80674d8ac397828af70350e4
liray-unendlich <15893314+liray-unendlich@users.noreply.github.com>
2024-05-01 16:06:35 +0900

    Add software upgrade/parameter change type to parse

--- services/telegram.js
+++ services/telegram.js
@@ -16,21 +16,32 @@ export async function sendMessageToTelegram(chatId, message) {
 
 export async function sendProposalToTelegram(proposal, chain) {
 	const cliMessage = `\`\`\`bash ${chain.binaryName} tx gov vote ${proposal.id} [yes/no/no_with_veto/abstain] --chain-id ${chain.chainId} --from [your_key_name]\`\`\``;
-	let messageText = `------------------------------\n**${chain.name}で新しいプロポーザルが提案されました!**\n${proposal.title}\n${proposal.description}`;
+
+	// proposal["@type"]の値によって分岐："/cosmos.gov.v1.MsgExecLegacyContent"や、"/cosmos.upgrade.v1beta1.MsgSoftwareUpgrade"がまずある。さらに、 MsgExecLegacyContentの場合は、proposal.content["@type"]の値によって分岐する。
+	let messageText = "";
+	if (proposal.type === "/cosmos.upgrade.v1beta1.MsgSoftwareUpgrade" || proposal.type === "/cosmos.upgrade.v1beta1.SoftwareUpgradeProposal") {
+		messageText += `*${chain.name}でソフトウェアアップグレードが提案されました!*\n`;
+		messageText += `\n*プロポーザルID*: ${proposal.id}\n`;
+		messageText += `*cosmovisorのフォルダ情報*: ${proposal.plan.name}\n`;
+		messageText += `*ブロック高*: ${proposal.plan.height}\n`;
+		messageText += `*備考*: ${proposal.plan.info}\n`;
+	} else if (proposal.type === "/cosmos.params.v1beta1.ParameterChangeProposal") {
+		messageText += `*${chain.name}でパラメータの変更が提案されました!*\n`;
+		messageText += `${proposal.title}\n${proposal.description}`;
+		messageText += `\n*プロポーザルID*: ${proposal.id}\n`;
+	} else {
+		messageText += `*${chain.name}で新しいプロポーザルが提案されました!*\n`;
+		messageText += `${proposal.title}\n${proposal.description}`;
+		messageText += `\n*プロポーザルID*: ${proposal.id}\n`;
+	}
+	messageText += `*提案時間*: ${format(proposal.submitTime, 'yyyy/MM/dd HH:mm')}\n`;
+	messageText += `*投票期間*: ${format(proposal.votingStartTime, 'yyyy/MM/dd HH:mm')} - ${format(proposal.votingEndTime, 'yyyy/MM/dd HH:mm')}\n`;
+	messageText += `*CLIでの投票方法*:\n${cliMessage}`;
+	messageText += `\n[プロポーザルリンク](${chain.proposalUrl.replace("{id}", proposal.id)})`;
 
 	// check the message length
 	if (messageText.length > 1024) {
 		messageText = messageText.substring(0, 1024) + " ...";
-	}
-
-	// add proposal information to message
-	messageText += `\n\n*プロポーザルID*: ${
-		proposal.id
-	}\n*提案時間*: ${format(proposal.submitTime, 'yyyy/MM/dd HH:mm')}\n*投票期間*: ${format(proposal.votingStartTime, 'yyyy/MM/dd HH:mm')} - ${format(proposal.votingEndTime, 'yyyy/MM/dd HH:mm')}\n*CLIでの投票方法*:\n${cliMessage}`;
-
-	if (chain.proposalUrl) {
-		const url = chain.proposalUrl.replace("{id}", proposal.id);
-		messageText += `\n\n[プロポーザルリンク](${url})`;
 	}
 
 	await sendMessageToTelegram(TgChannelId, messageText);
